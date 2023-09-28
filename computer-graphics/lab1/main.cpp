964dbd27ba28fafeed30afe5977967e8730858d0
Liam Junkermann <dev@ljsoft.xyz>
2023-09-28 15:07:22 +0100

    I made a rainbow

--- computer-graphics/lab1/main.cpp
+++ computer-graphics/lab1/main.cpp
@@ -12,26 +12,21 @@ const unsigned int SCR_HEIGHT = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
                                  "layout (location = 0) in vec3 aPos;\n"
+                                 "layout (location = 1) in vec3 aColor;\n"
+                                 "out vec3 outColor;\n"
                                  "void main()\n"
                                  "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                 "   gl_Position = vec4(aPos, 1.0);\n"
+                                 "   outColor = aColor;\n"
                                  "}\0";
-const char *fragmentShaderSource[] = {" \n\
-#version 330    \n\
-\n\
-in vec4 color;\n\
-out vec4 FragColor;\n\
-\n\
-void main()\n\
-{\n\
-FragColor = color;\n\
-}",
-                                      "#version 330 core\n"
-                                      "out vec4 FragColor;\n"
-                                      "void main()\n"
-                                      "{\n"
-                                      "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                      "}\n\0"};
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "in vec3 outColor;\n"
+                                   "void main()\n"
+                                   "{\n"
+                                   "   FragColor = vec4(outColor, 1.0f);\n"
+                                   "}\n\0";
 
 int main()
 {
@@ -113,9 +108,10 @@ int main()
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left
-        0.5f, -0.5f, 0.0f,  // right
-        0.0f, 0.5f, 0.0f    // top
+        // positions         // colors
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // top
     };
 
     unsigned int VBO, VAO;
@@ -127,8 +123,13 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
+    // color attribute
+    // location, size, size unit, do not normalize values, "stride" value (how many values we need to skip for each vertex), and the offset of each vertex for color
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
