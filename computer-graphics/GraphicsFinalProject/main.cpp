839b462a261685cd324918549340437d5630af64
Liam Junkermann <dev@ljsoft.xyz>
2023-12-14 12:09:55 +0000

    going 3d pt1

--- computer-graphics/GraphicsFinalProject/main.cpp
+++ computer-graphics/GraphicsFinalProject/main.cpp
@@ -3,6 +3,10 @@
 #include<GLFW/glfw3.h>
 #include<stb/stb_image.h>
 
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
@@ -11,16 +15,21 @@
 
 GLfloat vertices[] = {
 	// coords					/ colors			/ texture coords
-	-0.5f,	-0.5f,0.0f,			1.0f,0.0f,0.0f,		0.0f, 0.0f, // Lower left corner
-	-0.5f,	 0.5f,0.0f, 		0.0f,1.0f,0.0f,		0.0f, 1.0f, // Upper left corner
-	 0.5f,	 0.5f,0.0f, 		0.0f,0.0f,1.0f,		1.0f, 1.0f, // Upper right corner
-	 0.5f,	-0.5f,0.0f, 		1.0f,1.0f,1.0f,		1.0f, 0.0f, // Lower Left Corner
+	-0.5f,	0.0f,	 0.5f,		0.83f,0.70f,0.44f,		0.0f, 0.0f,
+	-0.5f,	0.0f,	-0.5f, 		0.83f,0.70f,0.44f,		5.0f, 0.0f,
+	 0.5f,	0.0f,	-0.5f, 		0.83f,0.70f,0.44f,		0.0f, 0.0f,
+	 0.5f,	0.0f,	 0.5f, 		0.83f,0.70f,0.44f,		5.0f, 0.0f,
+	 0.0f,	0.8f,	 0.0f, 		0.92f,0.86f,0.76f,		2.5f, 5.0f,
 };
 
 // order in which to render verticies
 GLuint indices[] = {
-	0,2,1, // top triangle
-	0,3,2 // bottom triangle
+	0,1,2,
+	0,2,3,
+	0,1,4,
+	1,2,4,
+	2,3,4,
+	3,0,4,
 };
 
 int main() {
@@ -32,7 +41,9 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "LJ Graphics", NULL, NULL);
+	int width = 800, height = 800;
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "LJ Graphics", NULL, NULL);
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
@@ -41,7 +52,7 @@ int main() {
 	glfwMakeContextCurrent(window);
 
 	gladLoadGL();
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
 
 	// Shader setup
 	Shader shaderProgram("default.vert", "default.frag");
@@ -61,22 +72,51 @@ int main() {
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	// Texture
-	Texture popCat("popcat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture brickTex("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	brickTex.texUnit(shaderProgram, "tex0", 0);
 
-	popCat.texUnit(shaderProgram, "tex0", 0);
+	float rotation = 0.0f;
+	double prevTime = glfwGetTime();
+	
+	glEnable(GL_DEPTH_TEST);
 
 	// main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		shaderProgram.Activate();
+
+		double crnTime = glfwGetTime();
+		if (crnTime - prevTime >= 1 / 30) {
+			rotation += 0.025f;
+			prevTime = crnTime;
+		}
+
+		// Initing 3d model/view/projection matrices
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
+		// translating view matrix down and away from us
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		// setting up the projection matrix (fov, aspect ratio, nearest visible distance, farthest visible distance)
+		proj = glm::perspective(glm::radians(45.0f), (float)(width / height), 0.1f, 100.0f);
+
+		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
 		glUniform1f(uniID, 0.5f);
-		popCat.Bind();
+		brickTex.Bind();
 		VAO1.Bind();
 
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(int), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
@@ -85,7 +125,7 @@ int main() {
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
-	popCat.Delete();
+	brickTex.Delete();
 	shaderProgram.Delete();
 
 	glfwDestroyWindow(window);
