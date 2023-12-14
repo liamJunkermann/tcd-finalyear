6d562c1b46b853bcb8502de4c7e925e5e5aafdb5
Liam Junkermann <dev@ljsoft.xyz>
2023-12-14 10:22:30 +0000

    basic 2d bits + texture implementation

---
+++ computer-graphics/GraphicsFinalProject/main.cpp
@@ -0,0 +1,94 @@
+#include<iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
+
+#include"shaderClass.h"
+#include"VAO.h"
+#include"VBO.h"
+#include"EBO.h"
+#include"texture.h"
+
+GLfloat vertices[] = {
+	// coords					/ colors			/ texture coords
+	-0.5f,	-0.5f,0.0f,			1.0f,0.0f,0.0f,		0.0f, 0.0f, // Lower left corner
+	-0.5f,	 0.5f,0.0f, 		0.0f,1.0f,0.0f,		0.0f, 1.0f, // Upper left corner
+	 0.5f,	 0.5f,0.0f, 		0.0f,0.0f,1.0f,		1.0f, 1.0f, // Upper right corner
+	 0.5f,	-0.5f,0.0f, 		1.0f,1.0f,1.0f,		1.0f, 0.0f, // Lower Left Corner
+};
+
+// order in which to render verticies
+GLuint indices[] = {
+	0,2,1, // top triangle
+	0,3,2 // bottom triangle
+};
+
+int main() {
+	// Init GLFW
+	glfwInit();
+
+	// Tell GLFW which version we're using
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 800, "LJ Graphics", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	gladLoadGL();
+	glViewport(0, 0, 800, 800);
+
+	// Shader setup
+	Shader shaderProgram("default.vert", "default.frag");
+	VAO VAO1;
+	VAO1.Bind();
+
+	VBO VBO1(vertices, sizeof(vertices));
+	EBO EBO1(indices, sizeof(indices));
+
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	VAO1.Unbind();
+	VBO1.Unbind();
+	EBO1.Unbind();
+
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
+	// Texture
+	Texture popCat("popcat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+
+	popCat.texUnit(shaderProgram, "tex0", 0);
+
+	// main while loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		
+		shaderProgram.Activate();
+		glUniform1f(uniID, 0.5f);
+		popCat.Bind();
+		VAO1.Bind();
+
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	VAO1.Delete();
+	VBO1.Delete();
+	EBO1.Delete();
+	popCat.Delete();
+	shaderProgram.Delete();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
+}
